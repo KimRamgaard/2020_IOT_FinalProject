21ad2602384512b85b7bee090a31fd3176c728f0
Kim Ramgaard <kim.ramgaard@gmail.com>
2020-12-28 13:16:13 +0100

    Correction to reverse proxy

--- back_end/src/server.js
+++ back_end/src/server.js
@@ -3,9 +3,6 @@ const morgan = require('morgan')
 const helmet = require('helmet')
 const cors = require('cors')
 //TODO might need to move
-const http = require('http')
-const https = require('https')
-const fs = require('fs')
 const mqtt = require('mqtt')
 
 
@@ -43,25 +40,13 @@ if (mqttClient.connected) {
   console.log('connected to MQTT server')
 }
 
-//TODO HTTP might need to move to frontend
-httpsKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH)
-httpsCert = fs.readFileSync(process.env.CERTIFICATE_PATH)
-
-const httpServer = http.createServer(app)
-const httpsServer = https.createServer({
-  cert: httpsCert,
-  key: httpsKey  
-}, app)
-
-//Socket.IO Init
-const io = require('socket.io')(httpsServer)
-
 //subscribe to all when connecting
 mqttClient.on('connect', function(){
   mqttClient.subscribe('#', function(err){
     console.log(err)
   })
 })
+
 
 //on recieved message
 mqttClient.on('message', function(topic, payload, packet){
@@ -71,31 +56,13 @@ mqttClient.on('message', function(topic, payload, packet){
 })
 
 //middleware
+
 app.use('/api/sensordata', sensorDataApi)
 
 app.use(middleware.notFound)
 app.use(middleware.errorHandler)
 
-//if request is http redirect to https
-//this should already happen from the server, but double safe is double good
-app.use((request, response, next) => {
-  if(request.secure){
-    next();
-  } else {
-    response.redirect(`https://${request.headers.host}${request.url}`)
-  }
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.APP_PORT}`)
 })
 
-httpServer.listen(process.env.HTTPPORT, () => {
-  console.log(`app is listening with http on port ${process.env.HTTPPORT}`)
-})
-
-httpsServer.listen(process.env.HTTPSPORT, () => {
-  console.log(`app is listening with http on port ${process.env.HTTPSPORT}`)
-})
-
-  /*
-app.listen(process.env.App_Port, () => {
-  console.log(`Example app listening at http://localhost:${process.env.App_Port}`)
-})
-*/
